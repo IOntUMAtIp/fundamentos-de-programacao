9cfdce87efd26c1c6a7b1e30e1b4485825c2fc9b
IOntUMAtIp <rhsolive@gmail.com>
2019-02-10 21:57:24 -0200

    Correção do problema g

--- Aula 01/problema g.py
+++ Aula 01/problema g.py
@@ -1,6 +1,6 @@
 import math
 
-a, b = map(float, input().split())
-c, d = map(float, input().split())
-resultado = (a - b) ** 2 + (c - d) ** 2
-print(math.sqrt(resultado))
+x1, y1 = map(float, input().split())
+x2, y2 = map(float, input().split())
+resultado = (x2 - x1) ** 2 + (y2 - y1) ** 2
+print("%.4f" % (math.sqrt(resultado)))

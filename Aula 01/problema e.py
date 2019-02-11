d417b1fa85f5116cda7d4f1425f999e39d617601
IOntUMAtIp <rhsolive@gmail.com>
2019-02-11 20:33:53 -0200

    Correção do problema g

--- Aula 01/problema e.py
+++ Aula 01/problema e.py
@@ -1,8 +1,8 @@
 total = int(input())
 
-anos = total / 365
-meses = (total / 30) % 12
-dias = (total % 365) % 30
+anos = int(total / 365)
+meses = int((total / 30) / 12)
+dias = int((total % 365) % 30)
 print('%d ano(s)' % anos)
 print('%d mes(es)' % meses)
 print('%d dia(s)' % dias)
